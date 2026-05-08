dac0e5684bda3cd2a9968783363ffd026ce13c0d
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-05-08 19:32:17 +0100

    Début de la partie front-end : Création de la COUCHE SERVICE: création des opérations transactionnelles, exceptions et séparation des bank-accounts 'CurrentAccount | SavingAccount'

---
+++ src/main/java/net/riyad/ebankingbackend/exceptions/BalanceNotSufficientException.java
@@ -0,0 +1,4 @@
+package net.riyad.ebankingbackend.exceptions;
+
+public class BalanceNotSufficientException {
+}
---
+++ src/main/java/net/riyad/ebankingbackend/services/BankAccountService.java
@@ -0,0 +1,4 @@
+package net.riyad.ebankingbackend.services;
+
+public interface BankAccountService {
+}
