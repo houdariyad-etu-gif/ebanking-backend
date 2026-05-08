61e46137f2edcf34c2e27c1b161a0d6c93edc13d
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-05-08 03:19:29 +0100

    Stratégie JOINED

--- src/main/java/net/riyad/ebankingbackend/entities/BankAccount.java
+++ src/main/java/net/riyad/ebankingbackend/entities/BankAccount.java
@@ -9,10 +9,10 @@ import net.riyad.ebankingbackend.enums.AccountStatus;
 import java.util.Date;
 import java.util.List;
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "TYPE" , length = 4)
+@Inheritance(strategy = InheritanceType.JOINED)
+//@DiscriminatorColumn(name = "TYPE" , length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
-public class BankAccount {
+public abstract class BankAccount {
     @Id
     private String id;
     private double balance;
